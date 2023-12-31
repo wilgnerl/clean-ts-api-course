175cf5c0196d62d3a685370743f5ddc9bbe7c111
wilgnerl <wilgnerl@al.insper.edu.br>
2023-12-31 18:08:11 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { type Express } from 'express';
+import { cors } from '../middlewares/cors';
 import { bodyParser } from '../middlewares/body-parser';
 
 export default (app: Express): void => {
   app.use(bodyParser);
+  app.use(cors);
 };

c37592a049ecf2fa5fc0dbf0f0fb5b15590003b6
wilgnerl <wilgnerl@al.insper.edu.br>
2023-12-31 16:40:52 -0300

    test: ensure BcryptAdapter throw if bcrypt throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -9,19 +9,35 @@ jest.mock('bcrypt', () => ({
   },
 }));
 
+const salt = 12;
+const makeSut = (): BcryptAdapter => {
+  const sut = new BcryptAdapter(salt);
+
+  return sut;
+};
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
-    const salt = 12;
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSut();
     const hashSpy = jest.spyOn(bcrypt, 'hash');
 
     await sut.encrypt('any_value');
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
   });
+  test('Should throw if bcrypt throws', async () => {
+    const sut = makeSut();
+
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const promise = sut.encrypt('any_value');
+
+    await expect(promise).rejects.toThrow();
+  });
   test('Should return a hash on success', async () => {
-    const salt = 12;
-    const sut = new BcryptAdapter(salt);
+    const sut = makeSut();
 
     const hash = await sut.encrypt('any_value');
 

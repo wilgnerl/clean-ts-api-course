e735f1e4745f11f11523df1a9c7eae89464c5b90
wilgnerl <wilgnerl@al.insper.edu.br>
2023-12-31 14:00:15 -0300

    test: ensure EmailValidatorAdapter return true if validator return true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,10 +1,24 @@
 import { EmailValidatorAdapter } from './email-validator';
+import validator from 'validator';
+
+jest.mock('validator', () => ({
+  isEmail(): boolean {
+    return true;
+  },
+}));
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator return false', () => {
     const sut = new EmailValidatorAdapter();
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
     const isValid = sut.isValid('invalid_mail@mail.com');
 
     expect(isValid).toBe(false);
+  });
+  test('Should return true if validator return true', () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid('valid_mail@mail.com');
+
+    expect(isValid).toBe(true);
   });
 });

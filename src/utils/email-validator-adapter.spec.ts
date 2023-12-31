ff936609fa67a3d9b88377146f1820c04c7514e2
wilgnerl <wilgnerl@al.insper.edu.br>
2023-12-31 14:02:26 -0300

    test: ensure validator call with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -21,4 +21,12 @@ describe('EmailValidator Adapter', () => {
 
     expect(isValid).toBe(true);
   });
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter();
+    const isEmailSpy = jest.spyOn(validator, 'isEmail');
+
+    sut.isValid('any_mail@mail.com');
+
+    expect(isEmailSpy).toHaveBeenCalledWith('any_mail@mail.com');
+  });
 });

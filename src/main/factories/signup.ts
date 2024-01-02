eff8228430382b3fbc3854a259f5e9f9c495ed04
wilgnerl <wilgnerl@al.insper.edu.br>
2024-01-02 18:17:08 -0300

    feat: add signup factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account';
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter';
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account';
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter';
+import { SignupController } from '../../presentation/controllers/signup/signup';
+
+export const makeSignupController = (): SignupController => {
+  const salt = 12;
+  const emailValidatorAdapter = new EmailValidatorAdapter();
+  const bcryptAdapter = new BcryptAdapter(salt);
+  const accountMongoRepository = new AccountMongoRepository();
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository);
+  return new SignupController(emailValidatorAdapter, dbAddAccount);
+};

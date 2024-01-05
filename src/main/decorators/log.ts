f8ab3fe6583621e02eed028c13d74591bf5aebef
wilgnerl <wilgnerl@al.insper.edu.br>
2024-01-05 15:16:06 -0300

    feat: ensure log mongo repository create an error log on success

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -14,7 +14,7 @@ export class LogControllerDecorator implements Controller {
     const httpResponse = await this.controller.handle(httpRequest);
     if (httpResponse.statusCode === 500) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      await this.logErrorRepository.log(httpResponse.body.stack);
+      await this.logErrorRepository.logError(httpResponse.body.stack);
     }
     return httpResponse;
   }

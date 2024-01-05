b391e810177c2f3e05da1ea45934e5f610c7113d
wilgnerl <wilgnerl@al.insper.edu.br>
2024-01-05 15:18:11 -0300

    refactor: move sut creation to a factory helper method

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -1,6 +1,11 @@
+import { type LogErrorRepository } from '../../../../data/protocols/log-error-repository';
 import { type Collection } from 'mongodb';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { LogMongoRepository } from './log';
+
+const makeSut = (): LogErrorRepository => {
+  return new LogMongoRepository();
+};
 
 describe('Log Mongo Repository', () => {
   let errorCollection: Collection;
@@ -15,9 +20,12 @@ describe('Log Mongo Repository', () => {
     await errorCollection.deleteMany();
   });
   test('Should create an error log on success', async () => {
-    const sut = new LogMongoRepository();
+    const sut = makeSut();
+
     await sut.logError('any_error');
+
     const count = await errorCollection.countDocuments();
+
     expect(count).toBe(1);
   });
 });

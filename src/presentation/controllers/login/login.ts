5cc58f3448163089964a0d02aaeffbb38216ac18
wilgnerl <wilgnerl@al.insper.edu.br>
2024-01-07 15:58:40 -0300

    feat: ensure loginConteroller call emailValidator with correct email

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,19 @@
 import { MissingParamError } from '../../errors';
 import { badRequest, ok } from '../../helpers/http-helper';
-import { type HttpRequest, type HttpResponse, type Controller } from '../../protocols';
+import {
+  type HttpRequest,
+  type HttpResponse,
+  type Controller,
+  type EmailValidator,
+} from '../../protocols';
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     if (!httpRequest.body.email) {
       return badRequest(new MissingParamError('email'));
@@ -10,6 +21,12 @@ export class LoginController implements Controller {
     if (!httpRequest.body.password) {
       return badRequest(new MissingParamError('password'));
     }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    this.emailValidator.isValid(httpRequest.body.email);
+    // if (!isValid) {
+    //   return badRequest(new InvalidParamError('email'));
+    // }
 
     return ok('');
   }

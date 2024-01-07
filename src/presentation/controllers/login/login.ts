22bf5aedce4050be7a9e1be3f2cfaa261e91e4e4
wilgnerl <wilgnerl@al.insper.edu.br>
2024-01-07 16:13:35 -0300

    feat: ensure LoginController calls authentication with correct value

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,3 +1,4 @@
+import { type Authentication } from '../../../domain/usecases/authentication';
 import { InvalidParamError, MissingParamError } from '../../errors';
 import { badRequest, ok, serverError } from '../../helpers/http-helper';
 import {
@@ -9,9 +10,11 @@ import {
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator;
+  private readonly authentication: Authentication;
 
-  constructor(emailValidator: EmailValidator) {
+  constructor(emailValidator: EmailValidator, authentication: Authentication) {
     this.emailValidator = emailValidator;
+    this.authentication = authentication;
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -29,6 +32,9 @@ export class LoginController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'));
       }
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      await this.authentication.auth(email, password);
 
       return ok('');
     } catch (error) {
